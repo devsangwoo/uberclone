390898eac4c42140322bfd93d58bb06e66821e30
sangwoo <dev.sangwoo@gmail.com>
2020-04-04 00:05:20 +0900

    [feat] 1.6 schema merged

---
+++ src/api/Hello/sayBye/sayBye.resolvers.ts
@@ -0,0 +1,7 @@
+const resolvers = {
+  Query: {
+    sayBye: () => 'hey hello how are ya',
+  },
+};
+
+export default resolvers;
---
+++ src/api/Hello/sayHello/sayHello.resolvers.ts
@@ -0,0 +1,7 @@
+const resolvers = {
+  Query: {
+    sayHello: () => 'hey hello how are ya',
+  },
+};
+
+export default resolvers;
